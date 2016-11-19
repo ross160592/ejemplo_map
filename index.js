c97746d7d11b28244a9f5f561c5da6f321a99da8
ross160592 <mayracruz_1992@hotmail.com>
2016-11-19 13:59:52 -0600

    Update index.js
    cambios en index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app =express();
-var htpp=require('http').server(app);
+var htpp=require('http').Server(app);
 // usando socketio
 var io =require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-var port= process.env.PORt || 3000;
+var port= process.env.PORT || 3000;
 
 app.get('/', function(req,res){
 	res.sendFile(__dirname + '/index.html');
