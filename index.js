f58354558b297f36dc7f1bbbdf9efc69fb58197d
ross160592 <mayracruz_1992@hotmail.com>
2016-11-26 10:34:08 -0600

    Add files via upload
    server configurado para enviar contador a los clientes

--- index.js
+++ index.js
@@ -16,3 +16,19 @@ app.get('/', function(req, res){
 http.listen(port,function(){
     console.log('Escuchando en el puerto: '+port);//mensaje en consola 
 });
+
+
+var socketCount=0; //contador de conexiones al Server
+io.on('connection',function(socket){
+	console.log('Usuario conectado...');
+	socketCount++;
+	io.sockets.emit('usuario conectado',socketCount);//emitiendo mensajes a todos los sockets o usuarios conectados
+
+
+});
+
+socket.on('disconnet',function(){
+	socketCount++;//decremento del contador
+	console.log('Usuario Desconectado');//mensaje en consola
+	io.sockets.emit('usuario desconectado');//mensaje a todos los sockets
+})
